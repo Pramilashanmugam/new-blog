48f4d50a6b2a882d8032392b4343c0aef423ab05
Pramila Shanmugam <pramila.sl@gmail.com>
2024-06-18 20:19:13 +0000

    updated urls.py

--- env.py
+++ env.py
@@ -3,3 +3,5 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgres://uuziledpxup:woLxZSFYGzZZ@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/carol_delay_fray_386935")
 os.environ.setdefault("SECRET_KEY","this-is-a-complicated-project")
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://<your_api_key>:<your_api_secret>@dnnaxvrug")
