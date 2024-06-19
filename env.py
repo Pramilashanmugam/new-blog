5f499bc947847f1f8e26c5630160d2d4e42d88d8
Pramila Shanmugam <pramila.sl@gmail.com>
2024-06-19 10:00:16 +0000

    updated cloudinary values

--- env.py
+++ env.py
@@ -3,5 +3,4 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgres://uuziledpxup:woLxZSFYGzZZ@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/carol_delay_fray_386935")
 os.environ.setdefault("SECRET_KEY","this-is-a-complicated-project")
-os.environ.setdefault(
-    "CLOUDINARY_URL", "cloudinary://<your_api_key>:<your_api_secret>@dnnaxvrug")
+os.environ.setdefault("CLOUDINARY_URL", "cloudinary://<714714289939727>:<p0CdkTFsiQspM5iJOtK167UBYHw>@dnnaxvrug")
